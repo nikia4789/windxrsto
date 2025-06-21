f7869f7ad190a633c113b632f8a86d4023d0a3fd
Nikia <new-task.snw2eym3p10o@app.goodday.work>
2025-06-21 23:49:02 +0000

    [WM] Script 'u/Nikia/scripta' deployed

--- u/Nikia/scripta.sh
+++ u/Nikia/scripta.sh
@@ -1,9 +1,27 @@
 # shellcheck shell=bash
 # arguments of the form X="$I" are parsed as parameters X of type string
-msg="$1"
-dflt="${2:-default value}"
 
-echo "$WEBPAGE"; set
+# import some environment variables from Variables
+for x in \
+ENCRYPTION_SHARED_KEY \
+GOODDAY_APITOKEN \
+GOODDAY_URL1 \
+GOODDAY_URL2 \
+GOODDAY_URL2_PROJECTID \
+GOODDAY_URL2_USERID \
+GOODDAY_URL3 \
+GOODDAY_URL4 \
+REDIS_KEY \
+REDIS_URL \
+WEBPAGE \
+
+do read "${x}" <  <(
+curl -s -H "Authorization: Bearer $WM_TOKEN" \
+  "$BASE_INTERNAL_URL/api/w/$WM_WORKSPACE/variables/get_value/u/Nikia/${x}" | jq -r .
+)
+#echo "$x": "${!x}" >&2
+done
+
 # the last line of the stdout is the return value
 # unless you write json to './result.json' or a string to './result.out'
 echo "Hello $msg"
